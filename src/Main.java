46040c49a1b8295c87c5902f6d6aebf288b47805
jfgg9 <42964637+jfgg9@users.noreply.github.com>
2019-12-16 12:33:32 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,25 @@ public class Main {
 
     public static void main(String[] args) {
         String emailAddress = "john.doe.mis2016@ie.edu";
-        String formattedEmailAddress = emailAddress.replaceAll(".", "/");
+        String formattedEmailAddress = emailAddress.replaceAll("\\.", "/");
 
         Random random = new Random();
         StringBuffer word = null;
 
-        switch (random.nextInt(2)) {
+        switch (random.nextInt(3)) {
             case 0:
+                System.out.println("first");
                 word = new StringBuffer('Y');
+                word.append('Y');
+                break;
             case 1:
+                System.out.println("second");
                 word = new StringBuffer('F');
+                break;
             case 2:
+                System.out.println("third");
                 word = new StringBuffer('T');
+                break;
         }
 
         word.append('o');
